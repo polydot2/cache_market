8a3b64b20819363cbe4ff58f3dffb3fcdbb66ddb
Poly <mini-sheep@hotmail.fr>
2022-12-16 13:59:58 +0100

    Update ai.py

--- ai.py
+++ ai.py
@@ -3,6 +3,7 @@ import requests
 import re
 import json
 import random
+import datetime
 
 def getFirst(url, index):
     print("GET for: " + url)
@@ -20,8 +21,7 @@ def getFirst(url, index):
     f.write(x.group(1))
     f.close()
 
-    # get first item
-
+    # get {index} item
     jsonGroup = json.loads(x.group(1))
     firstUrl = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['landingPage']['url']
     substring = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['price']['details']
@@ -31,7 +31,7 @@ def getFirst(url, index):
     url = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['landingPage']['url']
     image = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['images'][0]['physicalId']
     imageExt = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['images'][0]['extension']
-    image = 'https://m.media-amazon.com/images/I/'+image+imageExt
+    image = 'https://m.media-amazon.com/images/I/'+image+'.'+imageExt
     promotionWip = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['badge']['entity']['label']['content']['fragments']
     promotion = ''.join(list(map(lambda x: x['text'], promotionWip)))
 
@@ -39,7 +39,7 @@ def getFirst(url, index):
     priceCurrency = ""
     try:
         price = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['price']['details']['basisPrice']['moneyValueOrRange']['value']['amount']
-        price = price.replace('\n', '').replace(' ', '')
+        price = price.replace('\n', '').replace(' ', '').replace(',', '.').replace('\u202f', '').replace('\u00a0\u20ac', '')
         priceCurrency = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['price']['details']['basisPrice']['moneyValueOrRange']['value']['currencyCode']
     except KeyError:
         print("no basis price")
@@ -48,7 +48,7 @@ def getFirst(url, index):
     priceDealCurrency = ""
     try: 
         priceDeal = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['price']['details']['dealPrice']['moneyValueOrRange']['value']['amount']
-        priceDeal = priceDeal.replace('\n', '').replace(' ', '')
+        priceDeal = priceDeal.replace('\n', '').replace(' ', '').replace(',', '.').replace('\u202f', '').replace('\u00a0\u20ac', '')
         priceDealCurrency = jsonGroup['prefetchedData']['aapiGetDealsList'][0]['entities'][index]['entity']['details']['entity']['price']['details']['dealPrice']['moneyValueOrRange']['value']['currencyCode']
     except KeyError:
         print("no deal price")
@@ -94,10 +94,17 @@ def getFirst(url, index):
             promotionWip = firstItem.find_all('div', class_='oct-deal-badge-label')[0].find_all('span')
             promotion = ''.join(list(map(lambda x: x.text, promotionWip)))
 
+            priceDeal = ""
             priceDeal = firstItem.find_all('span', class_='a-offscreen')[0].text
-            priceDealCurrency = ""
-            price = firstItem.find_all('span', class_='a-text-strike')[0].text.replace('\n', '').replace(' ', '')
-            priceCurrency = ""
+            priceDeal = priceDeal.replace('\n', '').replace(' ', '').replace(',', '.').replace('\u202f', '').replace('\u00a0\u20ac', '')
+    
+            priceDealCurrency = firstItem.find_all('span', class_='a-price-symbol')[0].text.replace('\u20ac', 'EUR')
+    
+            price = ""
+            price = firstItem.find_all('span', class_='a-text-strike')[0].text
+            price = price.replace('\n', '').replace(' ', '').replace(',', '.').replace('\u202f', '').replace('\u00a0\u20ac', '')
+            
+            priceCurrency = firstItem.find_all('span', class_='a-price-symbol')[0].text.replace('\u20ac', 'EUR')
 
         except IndexError:
             print("not a collection")
@@ -127,6 +134,8 @@ def getFirst(url, index):
 def findAndAddItem(url, index): 
     try:
         item = getFirst(url, index)
+        item['date'] =  datetime.datetime.now()
+
         print(item['title'])
 
         if len(data['items']) > 0:
@@ -152,6 +161,8 @@ def pickHardAndAddTo(data):
 
     try:
         item = random.choice(hardData['items'])
+        item['date'] =  datetime.datetime.now()
+
         print(item['title'])
 
         if len(data['items']) > 0:
@@ -169,6 +180,11 @@ def pickHardAndAddTo(data):
     except Exception as e:
         print("error")
 
+
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
 #main
 
 game = 'https://www.amazon.fr/gp/goldbox?ie=UTF8&pf_rd_p=a1f294b4-3de9-4f2e-8561-165ecd4cc2af&pf_rd_r=4D301S34YY420TNQ7VME&pf_rd_s=auto-subnav-flyout-xiste-content-5&pf_rd_t=SubnavFlyout&ref_=sn_gfs_co_auto-xiste_51375011_1&deals-widget=%257B%2522version%2522%253A1%252C%2522viewIndex%2522%253A0%252C%2522presetId%2522%253A%2522deals-collection-all-deals%2522%252C%2522departments%2522%253A%255B%2522530490%2522%255D%252C%2522sorting%2522%253A%2522BY_SCORE%2522%257D'
@@ -183,21 +199,15 @@ f.close()
 
 findAndAddItem(game, 0)
 findAndAddItem(game, 1)
-findAndAddItem(game, 2)
-
 findAndAddItem(phone, 0)
-
 findAndAddItem(pc, 0)
-findAndAddItem(pc, 1)
-findAndAddItem(pc, 2)
-
 findAndAddItem(headphone, 0)
 
-pickHardAndAddTo(data)
+# pickHardAndAddTo(data)
 
 # remove old items
-data["items"] = data["items"][0:10]
+data["items"] = data["items"][0:20]
 
 f = open("sample.json", "w")
-f.write(json.dumps(data, indent=4))
+f.write(json.dumps(data, indent='\t', default = myconverter))
 f.close()
